0649bb6d97b7b801399a485ae8d6d2ae6aec521c
TexasTheNomad <62273746+TexasTheNomad@users.noreply.github.com>
2020-03-18 15:00:25 -0500

    Add files via upload

---
+++ Astrite_Ore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CosmicParallax.Items.Blocks
+{
+	public class Astrite_Ore : ModItem
+
+	{
+		public override void SetStaticDefaults()
+        {
+			DisplayName.SetDefault("Astrite Ore");
+        }
+		public override void SetDefaults()
+		{
+			item.width = 12;
+			item.height = 12;
+			item.rare = ItemRarityID.Blue;
+			item.value = Item.buyPrice(silver: 4);
+			item.maxStack = 999;
+		}
+	}
+}
